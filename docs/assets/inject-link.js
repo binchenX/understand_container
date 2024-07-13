baad38752109da2f73c1c139eeb73c807d5cc6e5
Bin Chen <pierr.chen@gmail.com>
2024-07-13 19:57:43 +1000

    add buy me coffee.

---
+++ docs/assets/inject-link.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var linkHtml = '<div class="custom-coffee-link-container"><a href="https://ko-fi.com/binchenx" target="_blank">Buy Me Coffee</a></div>';
+    document.body.insertAdjacentHTML('afterbegin', linkHtml);
+});
